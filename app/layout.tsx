fa2120417eebdbd3b701ff143775b27fc173a9f5
Rinataeva <rinataeva@gmail.com>
2025-02-19 13:42:51 +0300

    feat: tailwind, shadcn ui, inter-font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,12 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Header } from "../components/Header";
+import { Footer } from "../components/Footer";
+import { Inter } from "next/font/google";
 import "./globals.css";
+import { cn } from "@/lib/utils";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
+const inter = Inter({ subsets: ["latin"], variable: "--font-sans" });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -25,9 +21,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={cn("min-h-screen bg-background font-sans antialiased", inter.variable)}
       >
-        {children}
+        <Header />
+        <main className="min-h-screen">{children}</main>
+        <Footer />
       </body>
     </html>
   );
---
+++ app/user/[id]/page.tsx
@@ -0,0 +1,11 @@
+type Props = {
+    params: {
+        id: string;
+    };
+};
+
+export function generateMetadata({ params }: Props) {
+    return {
+        title: `User ${params.id}`,
+    };
+}
