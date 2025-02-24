58676f15cb10b5fbe23b7e90bf312b95ebd782b4
Rinataeva <rinataeva@gmail.com>
2025-02-24 10:13:06 +0300

    fix: added await for params cause params is async object

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <section className="py-10 flex flex-col items-center gap-5 text-center">
       <h1 className="text-2xl font-bold">
-        Welcome to the "Who Are Our Users?" Page!
+        Welcome to the &quot;Who Are Our Users?&quot; Page!
       </h1>
       <p className="text-lg text-muted-foreground">
         Let us dive into these quick insights
